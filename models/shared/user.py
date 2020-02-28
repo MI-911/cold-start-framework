b406bb5300121bf073e31d57a9c301e1d556bbb6
minibrams <anders@brams.dk>
2020-02-28 12:42:23 +0100

    Added warm/cold user classes and a cold-start user set class

--- models/shared/user.py
+++ models/shared/user.py
@@ -1,9 +1,41 @@
 
 
-class User:
+class WarmStartUser:
     """
-    The warmup() method in Recommenders take a list of User objects as input.
+    For the warmup() method of Recommenders.
     """
     def __init__(self, ratings, validation):
-        self.training = ratings  # { entity_id: sentiment }
-        self.validation = validation  # [ (positives, negative) ]
+        """
+        Initialises a warm-start user.
+        :param ratings: A dictionary of the format: { entity_id: sentiment }
+        :param validation: A pair of the format: (positive, negatives)
+        """
+        self.training = ratings
+        self.validation = validation
+
+
+class ColdStartUserSet:
+    def __init__(self, answers, positive, negatives):
+        """
+        Initialises a cold-start user set.
+        :param answers: A dictionary of the format: { entity_id: sentiment }
+        :param positive: An entity_id
+        :param negatives: A list of the format [ entity_id ]
+        """
+        self.answers = answers
+        self.positive = positive
+        self.negatives = negatives
+
+
+class ColdStartUser:
+    """
+    For the interview() and predict() methods of Recommenders.
+    """
+    def __init__(self, sets, validation):
+        """
+        Initialises a cold-start user.
+        :param sets: A list of the format [ ColdStartUserSet ]
+        :param validation: A pair of the format (positive, negatives)
+        """
+        self.sets = sets
+        self.validation = validation
