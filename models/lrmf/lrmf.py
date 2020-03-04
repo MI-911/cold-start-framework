453077776070bf89fb96b9db4e26ccbeb518a601
minibrams <anders@brams.dk>
2020-03-04 14:13:37 +0100

    Implemented decision tree structure

--- models/lrmf/lrmf.py
+++ models/lrmf/lrmf.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 
+LIKE = 1
+DISLIKE = 0
+
+
 class LRMF:
-    def __init__(self, l1, l2, n_users, n_entities, k):
+    def __init__(self, l1, l2, n_users, n_entities, kk):
         self.n_users = n_users
         self.n_entities = n_entities
         self.candidates = None
@@ -10,19 +14,27 @@ class LRMF:
         self.l1 = l1                                # Num. global questions
         self.l2 = l2                                # Num. local questions
 
-        self.l = l1 + l2 + 1                        # Question dimension
-        self.k = k                                  # Embedding dimension
+        self.k = l1 + l2 + 1                         # Question dimension
+        self.kk = kk                                 # Embedding dimension
 
-        self.R = np.zeros((n_users, n_entities))    # Ratings matrix
-        self.T = np.zeros((self.l, k))              # Transformation matrix
-        self.V = np.zeros((self.l, n_entities))     # Item embeddings
+        self.R = np.zeros((n_users, n_entities))     # Ratings matrix
+        self.T = np.zeros((self.k, kk))              # Transformation matrix
+        self.V = np.zeros((self.kk, n_entities))     # Item embeddings
 
-        self.l1_questions = [0 for _ in range(l1)]  # Indices of global questions
-        self.l2_questions = [0 for _ in range(l2)]  # Indices of local questions
+        # Regularisation
+        self.alpha = 0.001
+        self.beta = 0.001
 
-    def fit(self, training, candidates):
+    def fit(self, R, candidates):
+        self.R = R
         # 1. Build the tree
-        self.build_tree(training, 5)
+        tree = DecisionTree(
+            depth=self.l1 + self.l2,
+            users=[u for u in range(self.n_users)],
+            parent_interview=[],
+            LRMF=self)
+        tree.grow(candidates)
+        tree.show()
         # 2. Solve for user and transformation embeddings
         # 3. Solve for item embeddings
         pass
@@ -33,3 +45,60 @@ class LRMF:
         # 2. Split users into Like, Dislike, Unknown (explicit/implicit)
         # 3. Keep going until tree has a specific height
         pass
+
+    def solve_T(self, users):
+        # Solve the sylvester equation AX + XB = Q for X
+        pass
+
+
+class DecisionTree:
+    def __init__(self, depth, users, parent_interview, LRMF):
+        self.depth = depth
+        self.users = users
+        self.parent_interview = parent_interview
+        self.LRMF = LRMF
+        self.children = None
+        self.candidate = None
+
+    def grow(self, candidates):
+        if len(self.parent_interview) >= self.depth:
+            return
+        if len(self.users) == 0:
+            return
+
+        # Find split item
+        self.candidate = self.choose_candidate(candidates)
+        u_l, u_d = self.split_users(self.candidate)
+        interview = self.parent_interview + [self.candidate]
+
+        # Send users and interview to children
+        self.children = {
+            LIKE: DecisionTree(self.depth, u_l, interview, self.LRMF),
+            DISLIKE: DecisionTree(self.depth, u_d, interview, self.LRMF)
+        }
+
+        # Grow children recursively
+        candidates.remove(self.candidate)
+        self.children[LIKE].grow(candidates)
+        self.children[DISLIKE].grow(candidates)
+
+    def represent(self, users, questions):
+        return self.LRMF.R[users][:, questions]
+
+    def choose_candidate(self, candidates):
+        # TODO: Implement
+        return np.random.choice(candidates)
+
+    def split_users(self, candidate):
+        u_all = self.LRMF.R[self.users]
+        u_l, = np.where(u_all[:, candidate] == LIKE)
+        u_d, = np.where(u_all[:, candidate] == DISLIKE)
+
+        return u_l, u_d
+
+    def show(self):
+        indent = ''.join([' |' for _ in range(len(self.parent_interview))])
+        print(f'{indent} Node [q. {self.candidate}] ({len(self.users)} users)')
+        if self.children:
+            self.children[LIKE].show()
+            self.children[DISLIKE].show()
--- models/lrmf/lrmf_recommender.py
+++ models/lrmf/lrmf_recommender.py
@@ -1,6 +1,7 @@
+import pickle
 from typing import Dict, List
 
-from models.lrmf.lrmf import LRMF
+from models.lrmf.lrmf import LRMF, DecisionTree
 from models.shared.base_recommender import RecommenderBase
 from models.shared.user import WarmStartUser
 import numpy as np
@@ -16,10 +17,11 @@ def get_rating_matrix(training, n_users, n_entities, rating_map=None):
 
     R = np.zeros((n_users, n_entities))
     for user, data in training.items():
-        for entity, rating in data['training'].items():
+        for entity, rating in data.training.items():
             R[user, entity] = rating_map[rating]
 
     return R
+
 
 def extract_candidates(rating_matrix, n=100):
     n_ratings = rating_matrix.sum(axis=0)
@@ -33,27 +35,25 @@ class LRMFRecommender(RecommenderBase):
         self.meta = meta
         self.model = None
 
-        self.n_users = 0
-        self.n_entities = 0
-
         self.n_candidates = 100
 
+        self.n_users = len(meta.users)
+        self.n_entities = len(meta.entities)
+
     def warmup(self, training: Dict) -> None:
-        self.n_users = max(training.keys()) + 1
-        self.n_entities = len(self.meta['entities'])
-
-        self.model = LRMF(n_users=self.n_users, n_entities=self.n_entities, l1=3, l2=3, k=20)
-
         R = get_rating_matrix(training, self.n_users, self.n_entities, {
             1: 1,
             0: 0,
             -1: 0
         })
-        candidates = extract_candidates(R, n=self.n_candidates)
 
+        R = get_rating_matrix(training, self.n_users, self.n_entities)
+        candidates = extract_candidates(R, n=100)
+
+        self.model = LRMF(n_users=self.n_users, n_entities=self.n_entities, l1=3, l2=3, kk=20)
         self.model.fit(R, candidates)
 
-    def interview(self, answers: Dict[int, int], max_n_questions: int) -> List[int]:
+    def interview(self, answers: Dict[int, int], max_n_questions=5) -> List[int]:
         pass
 
     def predict(self, items: List[int], answers: Dict[int, int]) -> Dict[int, float]:
@@ -65,3 +65,11 @@ class LRMFRecommender(RecommenderBase):
     def load_params(self, params: Dict) -> None:
         pass
 
+
+if __name__ == '__main__':
+    training = pickle.load(open('../../partitioners/data/training.pkl', 'rb'))
+    testing = pickle.load(open('../../partitioners/data/testing.pkl', 'rb'))
+    meta = pickle.load(open('../../partitioners/data/meta.pkl', 'rb'))
+
+    recommender = LRMFRecommender(meta)
+    recommender.warmup(training)
