939ed3138e972c94a5054831c455d24cafd4cf2a
Anders Jakobsen <andersjkbsn@gmail.com>
2020-03-05 13:14:34 +0100

    Implemented pipeline

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd ..
-docker build -f Dockerfile.base -t mindreader/base .
 docker build -f Dockerfile.interview -t mindreader/interview .
 docker run --rm -v "${PWD}"/data:/app/data mindreader/interview --input data
