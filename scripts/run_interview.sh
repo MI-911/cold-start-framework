d06c48a7193b1feb270b76591da7025243ea52b9
Anders Jakobsen <andersjkbsn@gmail.com>
2020-03-06 13:20:09 +0100

    Ignore results

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
-docker run --rm -v "${PWD}"/data:/app/data -v "${PWD}"/.results:/app/results mindreader/interview --input data --include naive
+docker run --rm -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --include naive
