1cbd036f720df88b7f3279c8121ff4babf7044f7
tantefar <jendal@live.dk>
2020-03-15 19:28:29 +0100

    Add all entities to uri index and add triples path

--- models/shared/meta.py
+++ models/shared/meta.py
@@ -3,9 +3,10 @@ from typing import Dict, List
 
 class Meta:
     def __init__(self, entities: Dict[str, Dict], uri_idx: Dict[str, int], idx_item: Dict[int, bool],
-                 users: List[int], recommendable_entities: List[int]):
+                 users: List[int], recommendable_entities: List[int], triples_path: str):
         self.entities = entities
         self.uri_idx = uri_idx
         self.idx_item = idx_item
         self.users = users
         self.recommendable_entities = recommendable_entities
+        self.triples_path = triples_path
