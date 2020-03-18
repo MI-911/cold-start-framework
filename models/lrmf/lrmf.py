c57a05947fc6ee797ec9ccb177303ad28ad5ab4d
Minibrams <anders@brams.dk>
2020-03-18 17:04:42 +0100

    Optimising tree, starting optimising entity embeddings

--- models/lrmf/lrmf.py
+++ models/lrmf/lrmf.py
@@ -1,12 +1,16 @@
 from typing import List, Tuple, Union, Dict
 
 import numpy as np
-from scipy.linalg import solve_sylvester
+from scipy.linalg import solve_sylvester, inv, LinAlgError
 from models.lrmf.maxvol import py_rect_maxvol
 from tqdm import tqdm
 
 LIKE = 1
 DISLIKE = 0
+
+
+def except_item(lst, item):
+    return [i for i in lst if not i == item]
 
 
 def group_representation(users: List[int],
@@ -24,9 +28,42 @@ def split_users(users: List[int], entity: int, ratings: np.ndarray) -> Tuple[Lis
     return likes, dislikes
 
 
-def group_loss(users: List[int], entity_embeddings: np.ndarray, ratings: np.ndarray) -> float:
-    _R = ratings[users]
-    _B = group_representation(users, ratings)
+def global_questions_vector(questions: List[int], max_length: int) -> List[int]:
+    padding = [-1 for _ in range(max_length - len(questions))]
+    return questions + padding
+
+
+def local_questions_vector(candidates: List[int], entity_embeddings: np.ndarray, max_length: int) -> List[int]:
+    questions, _ = py_rect_maxvol(entity_embeddings[candidates], maxK=max_length)
+    return questions
+
+
+def transformation(users: List[int],
+                   representation: np.ndarray, entity_embeddings: np.ndarray,
+                   ratings: np.ndarray, alpha=1.0) -> np.ndarray:
+    try:
+        _A = representation.T @ representation
+        _B = alpha * inv(entity_embeddings.T @ entity_embeddings)
+        _Q = representation.T @ ratings[users] @ entity_embeddings @ inv(entity_embeddings.T @ entity_embeddings)
+        return solve_sylvester(_A, _B, _Q)
+    except LinAlgError:
+        return np.zeros((representation.shape[1], entity_embeddings.shape[1]))
+
+
+def group_loss(users: List[int], global_questions: List[int], local_questions: List[int],
+               entity_embeddings: np.ndarray, ratings: np.ndarray) -> float:
+    _B = group_representation(users, global_questions, local_questions, ratings)
+    _T = transformation(users, _B, entity_embeddings, ratings)
+
+    group_ratings = ratings[users]
+    predicted_ratings = _B @ _T @ entity_embeddings.T
+    loss = ((group_ratings - predicted_ratings) ** 2).sum()
+    regularisation = _T.sum() ** 2
+
+    return loss + regularisation
+
+
+def optimise_entity_embeddings(ratings: np.ndarray, tree) -> np.ndarray:
     pass
 
 
@@ -59,8 +96,14 @@ class LRMF:
         self.ratings: np.ndarray = np.zeros((n_users, n_entities))
         self.entity_embeddings: np.ndarray = np.zeros((n_entities, kk))
 
+        self.T = Tree(l1_questions=[], depth=0, max_depth=self.interview_length, lrmf=self)
+
     def fit(self, ratings: np.ndarray, candidates: List[int]):
-        pass
+        self.ratings = ratings
+
+        all_users = [u for u in range(self.n_users)]
+
+        self.T.grow(all_users, candidates)
 
     def validate(self, user: int, to_validate: List[int]) -> Dict[int, float]:
         pass
@@ -77,21 +120,17 @@ class Tree:
         self.depth = depth
         self.max_depth = max_depth
         self.l1_questions = l1_questions
+        self.l2_questions: List[int] = []
         self.lrmf = lrmf
 
         self.users: List[int] = []
+        self.transformation: Union[np.ndarray, None] = None
         self.question: Union[int, None] = None
+
+        self.children: Union[Dict[int, Tree], None] = None
 
     def is_leaf(self):
         return self.depth == self.max_depth
-
-    def global_questions(self) -> List[int]:
-        padding = [-1 for _ in range(self.max_depth - len(self.l1_questions))]
-        return self.l1_questions + padding
-
-    def local_questions(self, candidates) -> List[int]:
-        questions, _ = py_rect_maxvol(self.lrmf.entity_embeddings[candidates], maxK=self.lrmf.l2)
-        return questions
 
     def grow(self, users: List[int], candidates: List[int]):
         self.users = users
@@ -100,9 +139,43 @@ class Tree:
         for candidate in candidates:
             likes, dislikes = split_users(users, candidate, self.lrmf.ratings)
 
+            loss = 0
+            for group in [likes, dislikes]:
+                rest_candidates = except_item(candidates, candidate)
+                global_questions = global_questions_vector(self.l1_questions, self.max_depth)
+                local_questions = local_questions_vector(rest_candidates, self.lrmf.entity_embeddings, self.lrmf.l2)
+                loss += group_loss(
+                    group, global_questions, local_questions,self.lrmf.entity_embeddings, self.lrmf.ratings)
+
+            if loss < min_loss:
+                min_loss = loss
+                best_question = candidate
+
+        self.question = best_question
+        remaining_candidates = except_item(candidates, self.question)
+        self.l2_questions = local_questions_vector(
+            remaining_candidates, self.lrmf.entity_embeddings, self.lrmf.l2)
+
+        self.transformation = transformation(
+            self.users, group_representation(self.users, self.l1_questions, self.l2_questions, self.lrmf.ratings),
+            self.lrmf.entity_embeddings, self.lrmf.ratings)
+
+        self.children = {
+            LIKE: Tree(self.l1_questions + [self.question], self.depth + 1, self.max_depth, self.lrmf),
+            DISLIKE: Tree(self.l1_questions + [self.question], self.depth + 1, self.max_depth, self.lrmf)
+        }
+
+        likes, dislikes = split_users(self.users, self.question, self.lrmf.ratings)
+        self.children[LIKE].grow(likes, remaining_candidates)
+        self.children[DISLIKE].grow(dislikes, remaining_candidates)
 
     def interview_existing_user(self, user: int) -> np.ndarray:
-        pass
+        if self.is_leaf():
+            user_vector, = group_representation([user], self.l1_questions, self.l2_questions, self.lrmf.ratings)
+            return user_vector @ self.transformation
+
+        answer = self.lrmf.ratings[user, self.question]
+        return self.children[answer].interview_existing_user(user)
 
     def interview_new_user(self, answers: Dict[int, int]) -> Union[int, np.ndarray]:
         pass
