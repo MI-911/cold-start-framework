44528619280458f6e1a29ed2f88f3881cf969db8
Anders Jakobsen <andersjkbsn@gmail.com>
2020-04-09 16:28:07 +0200

    Run multiple, detached experiments

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
-docker run --rm -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug
+# docker run --rm -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug
+
+docker run --rm -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models top-pop random naive-ppr-collab naive-mf
+docker run --rm -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models naive-ppr-joint naive-ppr-kg
+docker run --rm -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models naive-ppr-joint naive-ppr-kg
+docker run --rm -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models fmf lrmf
+docker run --rm -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models melu
