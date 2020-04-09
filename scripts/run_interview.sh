01f4ed31663b7ec19dcb3c6b7448b372170da5e8
Anders Jakobsen <andersjkbsn@gmail.com>
2020-04-09 16:44:50 +0200

    Changed models flag to include

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -3,8 +3,8 @@ cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
 # docker run --rm -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug
 
-docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models top-pop random naive-ppr-collab naive-mf
-docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models naive-ppr-joint naive-ppr-kg
-docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models lrmf
-docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models fmf
+docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --include top-pop random naive-ppr-collab naive-mf
+docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --include naive-ppr-joint naive-ppr-kg
+docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --include lrmf
+docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --include fmf
 #docker run -d -v "${PWD}"/data:/app/data -v "${PWD}"/results:/app/results mindreader/interview --input data --debug --models melu
