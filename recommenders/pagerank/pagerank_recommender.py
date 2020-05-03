e10c61f492bd6f2eda593ba1418f62a5aeb84882
Anders Jakobsen <andersjkbsn@gmail.com>
2020-05-03 11:28:33 +0200

    Added 80/20/0 importance for PPR

--- recommenders/pagerank/pagerank_recommender.py
+++ recommenders/pagerank/pagerank_recommender.py
@@ -138,9 +138,10 @@ class PageRankRecommender(RecommenderBase):
 
         if not self.parameters:
             parameters = {
-                'alpha': np.arange(0.1, 1, 0.1),
+                'alpha': np.arange(0.1, 1, 0.15),
                 'importance': [
                     {1: 0.95, 0: 0.05, -1: 0.0},
+                    {1: 0.80, 0: 0.20, -1: 0.0},
                     {1: 1/3, 0: 1/3, -1: 1/3},
                     {1: 0.45, 0: 0.10, -1: 0.45}
                 ]
