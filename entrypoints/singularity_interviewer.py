6180198e00102c85c29837c10c9f976a57dff239
Theis <jendal@live.dk>
2020-05-03 12:39:11 +0200

    Test interviewer

---
+++ entrypoints/singularity_interviewer.py
@@ -0,0 +1,20 @@
+import argparse
+import subprocess
+
+from models.configuration import models
+from shared.utility import valid_dir
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--include', nargs='*', type=str, choices=models.keys(), help='models to include')
+parser.add_argument('--experiments', nargs='*', type=str, default=['default'], help='experiments to run')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    processes = []
+    for model in args.include:
+        tmp = ['python3.8', 'interview.py', '--include', model, '--experiment', *args.experiments]
+        processes.append(subprocess.Popen(['python3.8', 'interview.py', '--include', model, '--experiment',
+                                           *args.experiments], stdout=None, stderr=subprocess.STDOUT))
+
+    [p.wait() for p in processes]
