04fe0f8dd873d47eda228d48ec87eaadd454d422
Minibrams <anders@brams.dk>
2020-05-13 12:35:10 +0200

    Added logistic ranking loss

--- models/mcnet/mcnet_interviewer.py
+++ models/mcnet/mcnet_interviewer.py
@@ -5,11 +5,13 @@ from typing import List, Dict, Union
 from loguru import logger
 
 from models.base_interviewer import InterviewerBase
-from models.mcnet.mcnet_model import MonteCarloNet, hinge_loss
+from models.mcnet.mcnet_model import MonteCarloNet, hinge_loss, logistic_ranking_loss
 from shared.meta import Meta
 from shared.user import WarmStartUser
 import numpy as np
 import torch as tt
+import torch.nn as nn
+import matplotlib.pyplot as plt
 
 
 def get_rating_matrix(training: Dict[int, WarmStartUser], n_users, n_entities):
@@ -21,6 +23,17 @@ def get_rating_matrix(training: Dict[int, WarmStartUser], n_users, n_entities):
     return R
 
 
+def get_log_popularities(rating_matrix: np.ndarray):
+    log_popularities = np.zeros((rating_matrix.shape[1]))
+    for e, entity_rating_vector in enumerate(rating_matrix.T):
+        ratings, = np.where(entity_rating_vector != 0)
+        n_ratings = len(ratings)
+        # log_popularities[e] = np.log(n_ratings) if n_ratings else 0.0
+        log_popularities[e] = n_ratings
+
+    return log_popularities
+
+
 class MonteCarloNetInterviewer(InterviewerBase):
     def __init__(self, meta: Meta, use_cuda=False):
         super(MonteCarloNetInterviewer, self).__init__(meta)
@@ -30,48 +43,111 @@ class MonteCarloNetInterviewer(InterviewerBase):
         self.idx_uri_map = self.meta.get_idx_uri()
         self.log_popularities = np.zeros((self.n_entities,))
 
+        self.method = 'pairwise'
+        self.loss_functions = {
+            'pairwise': logistic_ranking_loss,
+            'prediction': nn.MSELoss()
+        }
+
+    def warmup(self, training: Dict[int, WarmStartUser], interview_length=5) -> None:
+        if self.method == 'pairwise':
+            self.warmup_pairwise(training, interview_length)
+        elif self.method == 'prediction':
+            self.warmup_prediction(training, interview_length)
+
     def warmup_pairwise(self, training: Dict[int, WarmStartUser], interview_length=5) -> None:
         R = get_rating_matrix(training, self.n_users, self.n_entities)
+        self.log_popularities = get_log_popularities(R)
 
-        for e, entity_rating_vector in enumerate(R.T):
-            ratings, = np.where(entity_rating_vector != 0)
-            n_ratings = len(ratings)
-            self.log_popularities[e] = np.log(n_ratings) if n_ratings else 0.0
+        self.model = MonteCarloNet(self.n_entities, hidden_dims=512, method=self.method)
 
-        self.model = MonteCarloNet(self.n_entities, hidden_dims=512)
-
-        loss_fnc = hinge_loss
+        loss_fnc = self.loss_functions[self.method]
         optimizer = tt.optim.Adam(self.model.parameters(), lr=0.001)
         best_model, best_score = None, 0
 
-        for epoch in range(10):
+        patience_counter = 0
+        max_patience = 10
+
+        epoch_losses = []
+        epoch_scores = []
+
+        for epoch in range(100):
             epoch_loss = tt.tensor(0.0).to(self.model.device)
             for user_ratings_vector in R:
+                # Copy the vector so we don't persist changes into the ratings matrix
+                user_ratings_vector = user_ratings_vector.copy()
                 # Pick, at random, one positive and one negative sample from the
                 # user's ratings.
-                # Construct the user vector from the remaining.
+                positive_ratings, = np.where(user_ratings_vector == 1)
+                unseen_ratings, = np.where(user_ratings_vector == 0)
+
+                if not len(positive_ratings):
+                    continue
+                pos = np.random.choice(positive_ratings)
+                neg = np.random.choice(unseen_ratings)
+
+                user_ratings_vector[pos] = 0
+
+                # Construct the user vector from the remaining ratings.
+                # user_vector = np.zeros((self.n_entities,), dtype=np.float32)
+                # user_vector[positive_ratings] = user_ratings_vector[remaining_entities]
+                user_vector = user_ratings_vector.astype(np.float32)
                 # Pass the user vector through the network and receive scores
-                # for all items. Calculate hinge loss (or pairwise ranking loss)
+                # for all items.
+                scores = self.model(user_vector)
+                # Calculate hinge loss (or pairwise ranking loss)
                 # for the positive and negative sample we drew before.
-                # Back propagate and repeat.
-                pass
+                loss = loss_fnc(scores[pos], scores[neg])
+                # Accumulate loss for back propagation and repeat.
+                epoch_loss += loss
 
-    def warmup_rmse(self, training: Dict[int, WarmStartUser], interview_length=5) -> None:
+            self.model.zero_grad()
+            epoch_loss.backward()
+            optimizer.step()
+
+            validation_score = self.validate(training, R)
+            logger.info(f'Epoch {epoch}: {epoch_loss / self.n_users} - validation score: {validation_score}')
+
+            epoch_losses.append(epoch_loss / self.n_users)
+            epoch_scores.append(validation_score * 10)
+
+            if validation_score > best_score:
+                best_model = pickle.loads(pickle.dumps(self.model))
+                best_score = validation_score
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                if patience_counter > max_patience:
+                    logger.info(f'Stopping training due to non-increasing validation for {max_patience} epochs.')
+                    break
+
+        logger.info(f'Using best model with validation score: {best_score}')
+        self.model = best_model
+
+        plt.plot(epoch_losses, label='Hinge loss f(x, y) = max(0, 1 - (x - y))^2')
+        plt.plot(epoch_scores,
+                 label=f'Validation '
+                       f'{self.meta.validator.metric}@{self.meta.validator.cutoff}'
+                       f' (scaled by 10 for comparison)')
+        plt.xlabel('Epochs')
+        plt.legend()
+        plt.show()
+
+    def warmup_prediction(self, training: Dict[int, WarmStartUser], interview_length=5) -> None:
         R = get_rating_matrix(training, self.n_users, self.n_entities)
+        self.log_popularities = get_log_popularities(R)
 
-        for e, entity_rating_vector in enumerate(R.T):
-            ratings, = np.where(entity_rating_vector != 0)
-            n_ratings = len(ratings)
-            self.log_popularities[e] = np.log(n_ratings) if n_ratings else 0.0
-
-        self.model = MonteCarloNet(self.n_entities, hidden_dims=512)
+        self.model = MonteCarloNet(self.n_entities, hidden_dims=512, method=self.method)
 
         loss_fnc = tt.nn.MSELoss()
         optimizer = tt.optim.Adam(self.model.parameters(), lr=0.001)
 
         best_model, best_score = None, 0
 
-        for epoch in range(10):
+        epoch_losses = []
+        epoch_scores = []
+
+        for epoch in range(100):
             epoch_loss = tt.tensor(0.0).to(self.model.device)
             for user_ratings_vector in R:
                 # Get the user's ratings (likes and dislikes)
@@ -104,6 +180,8 @@ class MonteCarloNetInterviewer(InterviewerBase):
 
             validation_score = self.validate(training, R)
             logger.info(f'Epoch {epoch}: {epoch_loss / self.n_users} - validation score: {validation_score}')
+            epoch_losses.append(epoch_loss / self.n_users)
+            epoch_scores.append(validation_score * 10)
 
             if validation_score > best_score:
                 best_model = pickle.loads(pickle.dumps(self.model))
@@ -111,6 +189,15 @@ class MonteCarloNetInterviewer(InterviewerBase):
 
         logger.info(f'Using best model with validation score: {best_score}')
         self.model = best_model
+
+        plt.plot(epoch_losses, label='MSE loss')
+        plt.plot(epoch_scores,
+                 label=f'Validation '
+                       f'{self.meta.validator.metric}@{self.meta.validator.cutoff}'
+                       f' (scaled by 10 for comparison)')
+        plt.xlabel('Epochs')
+        plt.legend()
+        plt.show()
 
     def validate(self, training: Dict[int, WarmStartUser], rating_matrix: np.ndarray):
         predictions = []
--- models/mcnet/mcnet_model.py
+++ models/mcnet/mcnet_model.py
@@ -3,12 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as ff
 
 
-def hinge_loss(first, second):
-    return (tt.relu(2 - (first - second)) ** 2).sum()
+def hinge_loss(positive, negative):
+    return (tt.relu(1 - (positive - negative)) ** 2).sum()
+
+
+def logistic_ranking_loss(positive, negative):
+    return tt.log(1 + tt.exp(-(positive - negative)))
 
 
 class MonteCarloNet(nn.Module):
-    def __init__(self, input_dims, hidden_dims):
+    def __init__(self, input_dims, hidden_dims, method):
         super(MonteCarloNet, self).__init__()
 
         self.fc1 = nn.Linear(input_dims, hidden_dims)
@@ -17,15 +21,17 @@ class MonteCarloNet(nn.Module):
 
         self.device = tt.device('cuda' if tt.cuda.is_available() else 'cpu')
         self.to(self.device)
+        self.prediction_activation = tt.sigmoid if method == 'pairwise' else tt.tanh
 
     def forward(self, user_ratings, dropout=True):
         user_ratings = tt.tensor(user_ratings).to(self.device)
 
         def apply_dropout(input, dropout):
-            return ff.dropout(input, p=0.5) if dropout else input
+            p = 0.5
+            return ff.dropout(input, p=p) if dropout else input
 
         x = tt.relu(apply_dropout(self.fc1(user_ratings), dropout))
         x = tt.relu(apply_dropout(self.fc2(x), dropout))
-        x = tt.tanh(apply_dropout(self.fc3(x), dropout))
+        x = self.prediction_activation(apply_dropout(self.fc3(x), dropout))
 
         return x
