72b3431a0fe3191ee86869f9e56802827eb012e9
Anders Jakobsen <andersjkbsn@gmail.com>
2020-05-23 01:39:52 +0200

    Altered run_interview example

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -4,9 +4,7 @@ cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
 
 EXPERIMENT="equal"
-BASE="/home/anders/Code/cold-start-framework/debug"
+BASE=${PWD}
 
-docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-ppr-joint --splits split_1 split_0
-docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-ppr-joint --splits split_2 split_3
-docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-ppr-joint --splits split_1 split_0 --recommendable
-docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-ppr-joint --splits split_2 split_3 --recommendable
+docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-joint
+docker run -d --rm -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-joint --recommendable
