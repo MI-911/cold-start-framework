051b6375e8810fd6820e7bd23c1a6304c957bb69
Anders Jakobsen <andersjkbsn@gmail.com>
2020-05-24 14:19:32 +0200

    Added back interview runner

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -3,10 +3,10 @@ bash build_base.sh
 cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
 
-EXPERIMENT="separation"
-BASE="/home/anders/Code/cold-start-framework/debug"
+EXPERIMENT="equal"
+BASE=${PWD}
 
-docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-mf
-docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-mf --recommendable
-docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-knn
-docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-knn --recommendable
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include pop-mf
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include pop-mf --recommendable
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include pop-mf
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include pop-mf --recommendable
