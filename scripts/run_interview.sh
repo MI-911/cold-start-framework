8ecafbf8477d1d723e04c7d01efceae47f445ced
Anders Jakobsen <andersjkbsn@gmail.com>
2020-06-03 21:01:54 +0200

    Reverted run interview changes

--- scripts/run_interview.sh
+++ scripts/run_interview.sh
@@ -4,10 +4,7 @@ cd ..
 docker build -f Dockerfile.interview -t mindreader/interview .
 
 EXPERIMENT="equal"
-BASE="/home/anders/Code/cold-start-framework/debug"
+BASE=${PWD}
 
-#docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-mf
-docker run -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-linear-learned --rec
-docker run -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-linear-learned
-#docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-knn
-#docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-adaptive-knn --recommendable
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-linear-joint-learned
+docker run -d -v $BASE/data:/app/data -v $BASE/results:/app/results mindreader/interview --upload --input data --output results --debug --experiments $EXPERIMENT --include greedy-ppr-linear-joint-learned --recommendable
